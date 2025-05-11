83812b55628ca7231f9a508c33d10bfd234bc7c6
qiaolu007 <lijunchn9@163.com>
2025-05-11 22:59:09 +0800

    ［需求／BUG］: nacos 注册中心 ［模块］: ［说明］: - 服务启动时就会注册自己的服务信息（服务名、IP、端口）到注册中心 - 调用者可以从注册中心订阅想要的服务，获取服务对应的实例列表（1个服务可能多实例部署） - 调用者自己对实例列表负载均衡，挑选一个实例 - 调用者向该实例发起远程调用 ［作者］: 桥泸007

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +16,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -42,6 +45,8 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 
 //    private final IItemService itemService;
     private final RestTemplate restTemplate;
+
+    private final DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -91,9 +96,14 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 2.查询商品
         // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
 
+        // 2.1 发现item-service服务的实例注册表
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("item-service");
+        // 2.2 负载均衡，挑选一个实例
+        ServiceInstance instance = instanceList.get(RandomUtil.randomInt(instanceList.size()));
+
         // 2.1.利用RestTemplate发起http请求，得到http的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
