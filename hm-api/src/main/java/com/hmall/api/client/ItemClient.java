080654ac8024ab846ae1b8139b880ed38fc480ef
qiaolu007 <lijunchn9@163.com>
2025-05-28 21:55:04 +0800

    ［需求／BUG］: 微服务保护：请求限流、线程隔离、服务熔断 ［模块］: ［说明］: ［作者］: 桥泸007

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -2,6 +2,7 @@ package com.hmall.api.client;
 
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
+import com.hmall.api.fallback.ItemClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Collection;
 import java.util.List;
 
-@FeignClient("item-service")
+@FeignClient(value = "item-service", fallbackFactory = ItemClientFallback.class)
 public interface ItemClient {
 
     @GetMapping("/items")
--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,5 +1,6 @@
 package com.hmall.api.config;
 
+import com.hmall.api.fallback.ItemClientFallback;
 import com.hmall.common.utils.UserContext;
 import feign.Logger;
 import feign.RequestInterceptor;
@@ -27,5 +28,10 @@ public class DefaultFeignConfig {
                 template.header("user-info", userId.toString());
             }
         };
+    }
+
+    @Bean
+    public ItemClientFallback itemClientFallback() {
+        return new ItemClientFallback();
     }
 }
